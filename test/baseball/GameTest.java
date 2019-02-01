ba02d1624bf5c0727d54b182177b3be7a05a63c8
youmebae <youme.bae@samsung.com>
2019-02-01 18:36:53 +0900

    테스트 케이스 예시

--- test/baseball/GameTest.java
+++ test/baseball/GameTest.java
@@ -7,8 +7,47 @@ import org.junit.Test;
 public class GameTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void 입력값이_없을_경우() {
 
+	}
+	
+	@Test
+	public void 입력값_자리수가_세자리가_아닐_경우() {
+
+	}
+	
+	@Test
+	public void 입력값에_숫자_외의_문자가_입력될_경우() {
+
+	}
+	
+	@Test
+	public void 입력값에_중복된_숫자가_입력될_경우() {
+
+	}
+	
+	@Test
+	public void 숫자_세개가_전부_일치_할_경우_3_strike() {
+
+	}
+	
+	@Test
+	public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
+
+	}
+	
+	@Test
+	public void 스트라이크만_있을_경우_1_strike_0_ball() {
+
+	}
+	
+	@Test
+	public void 볼만_있을_경우_0_strike_1_ball() {
+
+	}
+	
+	@Test
+	public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {
+
+	}
 }
