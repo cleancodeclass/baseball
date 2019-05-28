fa0e80e94b27781c59f9d449a5c8aac900f0de09
youme <youme.bae@gmail.com>
2019-05-28 15:27:25 +0900

    Update Game.java

--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -2,4 +2,25 @@ package baseball;
 
 public class Game {
 
+	public String question;
+
+	public GuessResult guess(String input) {
+		
+		if(input == null)	{
+			throw new IllegalArgumentException();
+		}
+		
+		if(input.equals(question))	{
+			GuessResult result = new GuessResult();
+			result.solved = true;
+			result.strike = 3;
+			result.ball = 0;
+			
+			return result;
+		}
+		
+		return null;
+		
+	}
+
 }
