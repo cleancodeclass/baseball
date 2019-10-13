48dcee6d52498f9e0ed4c18f96ac006f8263bff4
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 22:31:31 +0900

    Green-입력값이_없을_경우

--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -2,8 +2,10 @@ package baseball;
 
 public class Game {
 
-	public void guess(Object object) {
-		
+	public void guess(String guess) {
+		if(guess==null || guess.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
