cdd53e7ef4be6af7101a4b4cc708da53060834f3
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 23:10:16 +0900

    Refactor-Extract Method

--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -5,6 +5,37 @@ public class Game {
 	public String question;
 
 	public GuessResult guess(String guess) {
+		assertIllegalArgument(guess);
+		
+		if(isSolved(guess)) {
+			return createSolvedResult();
+		}else {
+			return createUnSolvedResult(guess);
+		}
+	}
+
+	private GuessResult createUnSolvedResult(String guess) {
+		int strikes = 0;
+		int balls = 0;
+		for(int index=0; index < question.length(); index++) {
+			if(question.indexOf(guess.charAt(index)) == index) {
+				strikes++;
+			}else if(question.indexOf(guess.charAt(index)) > -1) {
+				balls++;
+			}
+		}
+		return new GuessResult(false, strikes, balls);
+	}
+
+	private GuessResult createSolvedResult() {
+		return new GuessResult(true, 3, 0);
+	}
+
+	private boolean isSolved(String guess) {
+		return guess.equalsIgnoreCase(question);
+	}
+
+	private void assertIllegalArgument(String guess) {
 		if(guess==null || guess.isEmpty()) {
 			throw new IllegalArgumentException();
 		}
@@ -16,26 +47,15 @@ public class Game {
 				throw new IllegalArgumentException();
 			}
 		}
-		if(guess.charAt(0)==guess.charAt(1)
-				|| guess.charAt(0)==guess.charAt(2)
-				|| guess.charAt(1)==guess.charAt(2)) {
+		if(isDuplicatedNumber(guess)) {
 			throw new IllegalArgumentException();
 		}
-		
-		if(guess.equalsIgnoreCase(question)) {
-			return new GuessResult(true, 3, 0);
-		}else {
-			int strikes = 0;
-			int balls = 0;
-			for(int index=0; index < question.length(); index++) {
-				if(question.indexOf(guess.charAt(index)) == index) {
-					strikes++;
-				}else if(question.indexOf(guess.charAt(index)) > -1) {
-					balls++;
-				}
-			}
-			return new GuessResult(false, strikes, balls);
-		}
+	}
+
+	private boolean isDuplicatedNumber(String guess) {
+		return guess.charAt(0)==guess.charAt(1)
+				|| guess.charAt(0)==guess.charAt(2)
+				|| guess.charAt(1)==guess.charAt(2);
 	}
 
 }
